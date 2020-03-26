43181ae65808b3ff44fef1c712f29266c03c4df0
animesh-deka <animesh7637@gmail.com>
2020-03-26 22:51:21 +0530

    Revert "R171219006: Added Addition function"
    This reverts commit 1f2b14c8303e4ef67c79d3266ce924bcfc45ab4c.

--- calculator.cpp
+++ calculator.cpp
@@ -1,21 +1,17 @@
 #include <iostream>
 using namespace std;
 float multiply(float x, float y);
-float add(float x, float y);
 int main()
 {
         float number_1;
         float number_2;
         float product_of_numbers;
-	float sum_of_numbers;
         cout<<"Enter the first number"<<endl;
         cin>>number_1;
         cout<<"Enter the second number"<<endl;
 	cin>>number_2;
         product_of_numbers=multiply(number_1, number_2);
-	sum_of_numbers=add(number_1, number_2);
         cout<<"Product of two numbers"<<product_of_numbers;<<endl;
-	cout<<"sum of two numbers "<<sum_of_numbers<<endl;
         return 0;
 }
 float multiply(float x, float y)
@@ -23,9 +19,5 @@ float multiply(float x, float y)
         return(x*y);
 }
 
-float add(float x, float y)
-{
-    return (x+y);
-}
 
 
