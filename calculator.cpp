becbed296114a8e092c914f39cc352e94703ed40
ansh8tu <anshu8tu@gmail.com>
2020-03-27 18:20:42 +0530

    R171219006: Added Division function

--- calculator.cpp
+++ calculator.cpp
@@ -2,20 +2,24 @@
 using namespace std;
 float multiply(float x, float y);
 float add(float x, float y);
+float divide(float x, float y);
 int main()
 {
         float number_1;
         float number_2;
         float product_of_numbers;
 	float sum_of_numbers;
+	float division_of_numbers;
         cout<<"Enter the first number"<<endl;
         cin>>number_1;
         cout<<"Enter the second number"<<endl;
 	cin>>number_2;
         product_of_numbers=multiply(number_1, number_2);
 	sum_of_numbers=add(number_1, number_2);
+	division_of_numbers=divide(number_1,number_2);
         cout<<"Product of two numbers"<<product_of_numbers;<<endl;
 	cout<<"sum of two numbers "<<sum_of_numbers<<endl;
+	cout<<"Division of two numbers "<<division_of_numbers<<endl;
         return 0;
 }
 float multiply(float x, float y)
@@ -26,6 +30,19 @@ float multiply(float x, float y)
 float add(float x, float y)
 {
     return (x+y);
+}
+
+float divide(float x, float y)
+{
+	if(number_2==0)
+	{
+		cout<<"Please choose a different value for second number"<<endl;
+	}
+
+	else
+	{
+		cout<<number_1/number_2<<endl;
+	}
 }
 
 
